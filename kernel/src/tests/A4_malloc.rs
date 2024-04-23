0eb2540510af21906d942bddfdc7071cf5a5a188
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2024-04-23 17:33:45 +0200

    added a better malloc test

--- kernel/src/tests/A4_malloc.rs
+++ kernel/src/tests/A4_malloc.rs
@@ -16,12 +16,33 @@ fn test_malloc() {
 
 #[kernel_test]
 fn test_malloc_free() {
-    let mut rng = Rng::new(6437892);
-    let _ = malloc(0);
+    let mut rng = Rng::new(5674382);
+
+    let mut ptrs = [0 as *mut u8; 1024];
     for _ in 0..100 {
-        let ptr = malloc(rng.get(0, 10000) as usize);
-        unsafe {
-            free(ptr);
+        // create a random permutation
+        let mut perm = [0; 1024];
+        for i in 0..1024 {
+            perm[i] = i;
+        }
+        for i1 in 0..1024 {
+            let i2 = rng.get(0, 1024) as usize;
+            let temp = perm[i1];
+            perm[i1] = perm[i2];
+            perm[i2] = temp;
+        }
+
+        for i in 0..1024 {
+            let len = rng.get(0, 100);
+            ptrs[i] = malloc(len as usize);
+            unsafe {
+                memset(ptrs[i], 12, len as usize);
+            }
+        }
+        for i in 0..1024 {
+            unsafe {
+                free(ptrs[perm[i]]);
+            }
         }
     }
 }
