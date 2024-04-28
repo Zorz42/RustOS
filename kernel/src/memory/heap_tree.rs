6e814e141ae6c80736ea84ecd0f6cd243950224b
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2024-04-28 20:19:50 +0200

    finished heap tree (hopefully)

--- kernel/src/memory/heap_tree.rs
+++ kernel/src/memory/heap_tree.rs
@@ -33,7 +33,7 @@ impl HeapTree {
 
     fn allocate_pages(&self) {
         let from = self.get_base_ptr() as u64 / PAGE_SIZE;
-        let to = (self.get_base_ptr() as u64 + self.get_tree_size() as u64 * 3 + PAGE_SIZE - 1) / PAGE_SIZE;
+        let to = (self.get_base_ptr() as u64 + self.get_tree_size() as u64 * 4 + PAGE_SIZE - 1) / PAGE_SIZE;
         for page in from..to {
             map_page_auto((page * PAGE_SIZE) as VirtAddr, true, false);
         }
@@ -42,29 +42,31 @@ impl HeapTree {
     fn get_node_val(&self, node: u32) -> i32 {
         debug_assert!(node < 2 * self.get_tree_size());
         debug_assert!(node != 0);
-        unsafe { *self.tree_ptr.add(node as usize) as i32 - 1 }
+        unsafe {
+            *self.tree_ptr.add(node as usize) as i32 - 2
+        }
     }
 
     fn set_node_val(&mut self, node: u32, val: i32) {
         debug_assert!(val < (1 << 8) - 1);
-        debug_assert!(val >= -1);
+        debug_assert!(val >= -2);
         debug_assert!(node < 2 * self.get_tree_size());
         debug_assert!(node != 0);
         unsafe {
-            *self.tree_ptr.add(node as usize) = (val + 1) as u8;
+            *self.tree_ptr.add(node as usize) = (val + 2) as u8;
         }
     }
 
     /// Makes all regions free
     pub fn clear(&mut self) {
         unsafe {
-            memset(self.get_base_ptr(), 0, self.get_tree_size() as usize);
+            memset(self.get_base_ptr(), 0, self.get_tree_size() as usize * 2);
         }
         let mut curr = self.get_tree_size();
         let mut curr_val = 3;
         while curr != 0 {
             unsafe {
-                memset(self.get_node_ptr(curr), curr_val, curr as usize);
+                memset(self.get_node_ptr(curr), curr_val + 2, curr as usize);
             }
             curr_val += 1;
             curr /= 2;
@@ -75,7 +77,7 @@ impl HeapTree {
         if a == csize - 1 && b == csize - 1 {
             csize
         } else {
-            i32::max(a, b)
+            i32::max(i32::max(a, b), -1)
         }
     }
 
@@ -94,6 +96,11 @@ impl HeapTree {
         unsafe { self.get_base_ptr().add(idx as usize) }
     }
 
+    fn get_base_block2_ptr(&mut self, idx: u32) -> *mut u8 {
+        debug_assert!(idx < self.get_tree_size());
+        unsafe { self.get_base_ptr().add((idx + self.get_tree_size()) as usize) }
+    }
+
     /// Doubles its size
     fn double_size(&mut self) {
         let prev_base_ptr = self.get_base_ptr();
@@ -101,15 +108,16 @@ impl HeapTree {
         self.allocate_pages();
 
         unsafe {
-            memset(self.get_base_ptr(), 0, self.get_tree_size() as usize);
+            memset(self.get_base_ptr(), 0, self.get_tree_size() as usize * 2);
             memcpy(prev_base_ptr, self.get_base_ptr(), self.get_tree_size() as usize / 2);
+            memcpy(prev_base_ptr.add(self.get_tree_size() as usize / 2), self.get_base_ptr().add(self.get_tree_size() as usize), self.get_tree_size() as usize / 2);
         }
 
         let mut curr = self.get_tree_size();
         let mut curr_val = 3;
         while curr != 1 {
             unsafe {
-                memset(self.get_node_ptr(curr), curr_val, curr as usize);
+                memset(self.get_node_ptr(curr), curr_val + 2, curr as usize);
                 memcpy_non_aligned(self.get_node_ptr(curr / 2), self.get_node_ptr(curr), curr as usize / 2);
             }
             curr_val += 1;
@@ -129,7 +137,7 @@ impl HeapTree {
             return 3;
         }
 
-        for size in (0..=2).rev() {
+        for size in (1..=2).rev() {
             let size2 = 1 << size;
             let bits2: u32 = (1 << size2) - 1;
             for i in 0..8 / size2 {
@@ -138,7 +146,18 @@ impl HeapTree {
                 }
             }
         }
-        unreachable!();
+
+        return 0;
+    }
+
+    fn update_parents(&mut self, mut node: u32, mut csize_log2: u32) {
+        node /= 2;
+        csize_log2 += 1;
+        while node > 0 {
+            self.update_node(node, csize_log2);
+            node /= 2;
+            csize_log2 += 1;
+        }
     }
 
     /// Finds a new region of the size: 2^size
@@ -156,18 +175,13 @@ impl HeapTree {
         let mut r = self.size;
 
         loop {
+            debug_assert!(self.get_node_val(node) >= size_log2 as i32);
+
             if r - l == size {
                 // this node will be allocated
-                self.set_node_val(node, -1);
+                self.set_node_val(node, -2);
 
-                // update all parent nodes
-                let mut csize_log2 = size_log2 + 1;
-                node /= 2;
-                while node != 0 {
-                    self.update_node(node, csize_log2);
-                    node /= 2;
-                    csize_log2 += 1;
-                }
+                self.update_parents(node, size_log2);
                 return l;
             }
 
@@ -193,19 +207,14 @@ impl HeapTree {
             if (bitmask & (bits << (i * size))) == 0 {
                 unsafe {
                     *self.get_base_block_ptr(idx) |= (bits << (i * size)) as u8;
+                    debug_assert_eq!(*self.get_base_block2_ptr(idx) as u32 & (bits << (i * size)), 0);
+                    *self.get_base_block2_ptr(idx) |= (1 << (i * size)) as u8;
                 }
 
                 let node_val = Self::get_biggest_segment(unsafe { *self.get_base_block_ptr(idx) as u32 });
                 self.set_node_val(node, node_val);
 
-                // update all parent nodes
-                let mut csize_log2 = 4;
-                node /= 2;
-                while node != 0 {
-                    self.update_node(node, csize_log2);
-                    node /= 2;
-                    csize_log2 += 1;
-                }
+                self.update_parents(node, 3);
 
                 return 8 * idx + i * size;
             }
@@ -216,5 +225,66 @@ impl HeapTree {
 
     /// Frees a region at the position
     /// If no region is reserved there, it panics
-    pub fn free(&mut self, pos: u32) {}
+    pub fn free(&mut self, pos: u32) {
+        let mut node = 1;
+        let mut l = 0;
+        let mut r = self.size;
+
+        loop {
+            if self.get_node_val(node) == -2 {
+                debug_assert_eq!(l, pos);
+                if node >= self.get_tree_size() {
+                    self.set_node_val(node, 3);
+                    debug_assert_eq!(unsafe { *self.get_base_block_ptr(node - self.get_tree_size()) }, 0);
+                    debug_assert_eq!(unsafe { *self.get_base_block2_ptr(node - self.get_tree_size()) }, 0);
+                } else {
+                    debug_assert_eq!(self.get_node_val(2 * node), self.get_node_val(2 * node + 1));
+                    let val = self.get_node_val(2 * node) + 1;
+                    debug_assert_eq!(1 << val, r - l);
+                    self.set_node_val(node, val);
+                }
+
+                self.update_parents(node, self.get_node_val(node) as u32);
+
+                return;
+            }
+
+            if node >= self.get_tree_size() {
+                break;
+            }
+
+            let mid = (l + r) / 2;
+            if pos < mid {
+                node = 2 * node;
+                r = mid;
+            } else {
+                node = 2 * node + 1;
+                l = mid;
+            }
+        }
+
+        // in this case, we need to deal with bitmask blocks
+        let bit_pos = pos % 8;
+        let mut block1 = unsafe { *self.get_base_block_ptr(node - self.get_tree_size()) as u32 };
+        let mut block2 = unsafe { *self.get_base_block2_ptr(node - self.get_tree_size()) as u32 };
+        assert_ne!((block2 & (1 << bit_pos)), 0);
+        block2 &= !(1 << bit_pos);
+        let mut curr_pos = bit_pos;
+        while curr_pos < 8 && (block2 & (1 << curr_pos)) == 0 {
+            block1 &= !(1 << curr_pos);
+            curr_pos += 1;
+        }
+
+        debug_assert!(block1 < (1 << 8));
+        debug_assert!(block2 < (1 << 8));
+
+        unsafe {
+            *self.get_base_block_ptr(node - self.get_tree_size()) = block1 as u8;
+            *self.get_base_block2_ptr(node - self.get_tree_size()) = block2 as u8;
+        }
+
+        let node_val = Self::get_biggest_segment(block1);
+        self.set_node_val(node, node_val);
+        self.update_parents(node, 3);
+    }
 }
