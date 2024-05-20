f2fe76bb7285b53d25a858d98fb912223952aa61
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2024-05-20 22:33:35 +0200

    adding performance tests

--- kernel/src/tests/A1_utils.rs
+++ kernel/src/tests/A1_utils.rs
@@ -1,10 +1,8 @@
 use kernel_test::{kernel_test, kernel_test_mod};
 
 use std::swap;
-#[cfg(feature = "run_tests")]
 use std::{memcpy, memcpy_non_aligned, memset, memset_int64, Rng};
 
-#[cfg(feature = "run_tests")]
 use super::get_free_space_addr;
 
 kernel_test_mod!(crate::tests::A1_utils);
--- kernel_test/src/lib.rs
+++ kernel_test/src/lib.rs
@@ -4,6 +4,7 @@ use std::ops::Add;
 use syn::ItemFn;
 
 static mut TESTS: Vec<String> = Vec::new();
+static mut PERF_TESTS: Vec<String> = Vec::new();
 static mut CURR_MOD: String = String::new();
 
 #[proc_macro_attribute]
@@ -16,7 +17,6 @@ pub fn kernel_test(_args: TokenStream, input: TokenStream) -> TokenStream {
 
     let code = format!(
         r#"
-        #[cfg(feature = "run_tests")]
         pub {function}
     "#
     );
@@ -28,12 +28,49 @@ pub fn kernel_test(_args: TokenStream, input: TokenStream) -> TokenStream {
     code.parse().expect("Generated invalid tokens")
 }
 
+#[proc_macro_attribute]
+pub fn kernel_perf(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let function = input.to_string();
+
+    let input_fn = syn::parse_macro_input!(input as ItemFn);
+    let test_fn = input_fn.sig.ident;
+    let function_full_name = unsafe { format!("{CURR_MOD}::{test_fn}") };
+
+    let code = format!(
+        r#"
+        pub {function}
+    "#
+    );
+
+    unsafe {
+        PERF_TESTS.push(function_full_name.to_string());
+    }
+
+    code.parse().expect("Generated invalid tokens")
+}
+
 #[proc_macro]
 pub fn all_tests(_item: TokenStream) -> TokenStream {
     let mut code = "[".to_owned();
 
     unsafe {
         for test in TESTS.iter() {
+            let function_name = test.split(':').last().unwrap();
+            code = code.add(&format!("({test} as fn(), \"{function_name}\"),"));
+        }
+    }
+
+    code.push(']');
+
+    code.parse().expect("Generated invalid tokens")
+}
+
+#[proc_macro]
+pub fn all_perf_tests(_item: TokenStream) -> TokenStream {
+    let mut code = "[".to_owned();
+
+    unsafe {
+        for test in PERF_TESTS.iter() {
             let function_name = test.split(':').last().unwrap();
             code = code.add(&format!("({test} as fn(), \"{function_name}\"),"));
         }
