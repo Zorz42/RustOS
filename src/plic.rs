57b44128231b9f37f2a24f8dc57ea412f0621292
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2024-06-06 14:29:38 +0200

    added volatile to plic

--- src/plic.rs
+++ src/plic.rs
@@ -4,32 +4,34 @@ const PLIC_BASE: u64 = 0x0c000000;
 
 pub fn plicinit() {
     for irq in 0..=8 {
+        let addr = (PLIC_BASE + 4 * irq) as *mut u32;
         unsafe {
-            *(PLIC_BASE as *mut u32).add(irq) = 1;
+            addr.write_volatile(1);
         }
     }
 }
 
 pub fn plicinithart() {
     let val = 0b111111111;
-    let addr1 = PLIC_BASE + 0x2080 + get_core_id() * 0x100;
+    let addr1 = (PLIC_BASE + 0x2080 + get_core_id() * 0x100) as *mut u32;
     unsafe {
-        *(addr1 as *mut u32) = val;
+        addr1.write_volatile(val);
     }
 
-    let addr2 = PLIC_BASE + 0x201000 + get_core_id() * 0x2000;
+    let addr2 = (PLIC_BASE + 0x201000 + get_core_id() * 0x2000) as *mut u32;
     unsafe {
-        *(addr2 as *mut u32) = 0;
+        addr2.write_volatile(0);
     }
 }
 
 pub fn plic_irq() -> u32 {
-    let addr = PLIC_BASE + 0x201004 + get_core_id() * 0x2000;
-    unsafe { *(addr as *mut u32) }
+    let addr = (PLIC_BASE + 0x201004 + get_core_id() * 0x2000) as *mut u32;
+    unsafe { addr.read_volatile() }
 }
 
 pub fn plic_complete(irq: u32) {
+    let addr = (PLIC_BASE + 0x201004 + get_core_id() * 0x2000) as *mut u32;
     unsafe {
-        *((PLIC_BASE + 0x201004 + get_core_id() * 0x2000) as *mut u32) = irq;
+        addr.write_volatile(irq);
     }
 }
