37f6ff677b1bf55c23531a00767f9af93905e957
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2024-10-03 16:36:55 +0200

    it now detects elf header and its sections

--- kernel/src/program_runner.rs
+++ kernel/src/program_runner.rs
@@ -1,9 +1,131 @@
-use std::{println, String};
+use std::{println, String, Vec};
 use crate::disk::filesystem::get_fs;
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct ElfHeader {
+    pub magic: [u8; 4],
+    pub bits: u8,
+    pub endianness: u8,
+    pub version: u8,
+    pub abi: u8,
+    pub abi_version: u8,
+    pub padding: [u8; 7],
+    pub elf_type: u16,
+    pub machine: u16,
+    pub version2: u32,
+    pub entry: u64,
+    pub ph_offset: u64,
+    pub sh_offset: u64,
+    pub flags: u32,
+    pub header_size: u16,
+    pub ph_entry_size: u16,
+    pub ph_entry_count: u16,
+    pub sh_entry_size: u16,
+    pub sh_entry_count: u16,
+    pub sh_str_index: u16,
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct ElfProgramHeader {
+    pub p_type: u32,
+    pub flags: u32,
+    pub offset: u64,
+    pub vaddr: u64,
+    pub paddr: u64,
+    pub file_size: u64,
+    pub memory_size: u64,
+    pub align: u64,
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct ElfSectionHeader {
+    pub name: u32,
+    pub s_type: u32,
+    pub flags: u64,
+    pub addr: u64,
+    pub offset: u64,
+    pub size: u64,
+    pub link: u32,
+    pub info: u32,
+    pub addr_align: u64,
+    pub entry_size: u64,
+}
+
+fn verify_elf_header(header: &ElfHeader) -> bool {
+    if header.magic != [0x7f, 0x45, 0x4c, 0x46] {
+        return false;
+    }
+
+    if header.bits != 2 {
+        return false;
+    }
+
+    if header.endianness != 1 {
+        return false;
+    }
+
+    if header.version != 1 {
+        return false;
+    }
+
+    if header.abi != 0 {
+        return false;
+    }
+
+    if header.abi_version != 0 {
+        return false;
+    }
+
+    if header.elf_type != 2 {
+        return false;
+    }
+
+    if header.machine != 0xf3 {
+        return false;
+    }
+
+    if header.version2 != 1 {
+        return false;
+    }
+
+    true
+}
 
 pub fn run_program(path: &String) {
     println!("Running program: {}", path);
 
     let program = get_fs().get_file(path).unwrap().read();
+    let elf_header = unsafe { (program.as_ptr() as *const ElfHeader).read() };
 
+    if !verify_elf_header(&elf_header) {
+        println!("Invalid ELF header");
+        return;
+    }
+
+    // get program headers
+    let mut program_headers = Vec::new();
+    for i in 0..elf_header.ph_entry_count {
+        let program_header = unsafe { (program.as_ptr().add(elf_header.ph_offset as usize) as *const ElfProgramHeader).add(i as usize).read() };
+        program_headers.push(program_header);
+    }
+
+    // get section headers
+    let mut section_headers = Vec::new();
+    for i in 0..elf_header.sh_entry_count {
+        let section_header = unsafe { (program.as_ptr().add(elf_header.sh_offset as usize) as *const ElfSectionHeader).add(i as usize).read() };
+        section_headers.push(section_header);
+    }
+
+    println!("Elf header: {:?}", elf_header);
+    println!("Program headers: ");
+    for header in &program_headers {
+        println!("{:?}", header);
+    }
+    println!("Section headers: ");
+    for header in &section_headers {
+        println!("{:?}", header);
+    }
 }
