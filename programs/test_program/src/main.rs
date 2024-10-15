03a92cb2ad84a99cfb1492a652395e93dfd67fdc
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2024-10-15 20:28:14 +0200

    created a separate std for programs

--- programs/test_program/src/main.rs
+++ programs/test_program/src/main.rs
@@ -1,85 +1,25 @@
 #![no_std]
 #![no_main]
 
-use core::arch::{asm, global_asm};
-use core::fmt;
-use core::fmt::Write;
+use core::arch::global_asm;
 use core::panic::PanicInfo;
-use kernel_std::{init_print, print, println};
+use std::{exit, get_pid, get_ticks, println};
 
-global_asm!(include_str!("asm/entry.S"));
+global_asm!(r#"
+.section .init
 
-fn syscall0(code: u64) {
-    unsafe {
-        asm!("ecall", in("a7") code);
-    }
-}
+_start:
+    j rust_entry
 
-fn syscall1(code: u64, arg1: u64) {
-    unsafe {
-        asm!("ecall", in("a7") code, in("a3") arg1);
-    }
-}
+"#);
 
-fn syscall2(code: u64, arg1: u64, arg2: u64) {
-    unsafe {
-        asm!("ecall", in("a7") code, in("a3") arg1, in("a4") arg2);
-    }
-}
-
-fn syscall0r(code: u64) -> u64 {
-    let ret: u64;
-    unsafe {
-        asm!("ecall", in("a7") code, out("a2") ret);
-    }
-    ret
-}
-
-fn print_str(s: &str) {
-    syscall2(1, s.as_ptr() as u64, s.len() as u64);
-}
-
-fn get_ticks() -> u64 {
-    syscall0r(2)
-}
-
-fn get_pid() -> u64 {
-    syscall0r(3)
-}
-
-struct Writer;
-
-impl Write for Writer {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        print_str(s);
-        Ok(())
-    }
-}
-
-#[doc(hidden)]
-pub fn _print(args: fmt::Arguments) {
-    let mut writer = Writer;
-    writer.write_fmt(args).unwrap();
-}
-
-pub fn exit() -> ! {
-    syscall0(4);
-    loop {}
+#[no_mangle]
+extern "C" fn rust_entry() -> ! {
+    std::init();
 }
 
 #[no_mangle]
-fn rust_entry() -> ! {
-    init_print(&_print);
-
-    main();
-
-    exit();
-}
-
-const ARRAY_SIZE: usize = 100000;
-static mut ARRAY: [u32; ARRAY_SIZE] = [0; ARRAY_SIZE];
-
-pub fn main() {
+pub extern "C" fn main() {
     println!("Hello, world!");
 
     let mut curr_ticks = get_ticks() / 1000;
@@ -97,7 +37,5 @@ pub fn main() {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("panic: {}", info);
-    loop {
-
-    }
+    exit();
 }
---
+++ std/src/lib.rs
@@ -0,0 +1,80 @@
+#![no_std]
+use core::arch::asm;
+use core::fmt;
+use core::fmt::Write;
+use kernel_std::init_print;
+
+pub use kernel_std::{print, println};
+
+
+extern "C" {
+    fn main();
+}
+
+pub fn init() -> ! {
+    init_print(&_print);
+
+    unsafe {
+        main();
+    }
+
+    exit();
+}
+
+fn syscall0(code: u64) {
+    unsafe {
+        asm!("ecall", in("a7") code);
+    }
+}
+
+fn syscall1(code: u64, arg1: u64) {
+    unsafe {
+        asm!("ecall", in("a7") code, in("a3") arg1);
+    }
+}
+
+fn syscall2(code: u64, arg1: u64, arg2: u64) {
+    unsafe {
+        asm!("ecall", in("a7") code, in("a3") arg1, in("a4") arg2);
+    }
+}
+
+fn syscall0r(code: u64) -> u64 {
+    let ret: u64;
+    unsafe {
+        asm!("ecall", in("a7") code, out("a2") ret);
+    }
+    ret
+}
+
+fn print_str(s: &str) {
+    syscall2(1, s.as_ptr() as u64, s.len() as u64);
+}
+
+pub fn get_ticks() -> u64 {
+    syscall0r(2)
+}
+
+pub fn get_pid() -> u64 {
+    syscall0r(3)
+}
+
+struct Writer;
+
+impl Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        print_str(s);
+        Ok(())
+    }
+}
+
+#[doc(hidden)]
+fn _print(args: fmt::Arguments) {
+    let mut writer = Writer;
+    writer.write_fmt(args).unwrap();
+}
+
+pub fn exit() -> ! {
+    syscall0(4);
+    loop {}
+}
